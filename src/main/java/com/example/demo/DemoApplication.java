1482d85cf9e6b9e06cf0c7d0b3a62c003df41f92
pramodBhanaka <bhanaka94pramod@gmail.com>
2023-09-21 11:12:13 +0530

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//	test remote branch
+//	test remote branchd
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
