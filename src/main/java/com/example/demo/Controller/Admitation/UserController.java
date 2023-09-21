27d4b4365d1c8517e78ce4b7b4f61fd1e22abc3c
pramodBhanaka <bhanaka94pramod@gmail.com>
2023-09-21 15:56:32 +0530

    create a retrieve all data from user role details

--- src/main/java/com/example/demo/Controller/Admitation/UserController.java
+++ src/main/java/com/example/demo/Controller/Admitation/UserController.java
@@ -1,11 +1,14 @@
 package com.example.demo.Controller.Admitation;
 
 import com.example.demo.Models.Admintation.UserRole;
+import com.example.demo.Repository.Admintation.UserDetailsRepository.UserRoleRepository;
 import com.example.demo.Service.Admintation.UserDetails.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -13,14 +16,19 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserDetailsService userDetailsService ;
+    @Autowired
+    private UserRoleRepository userRoleRepository ;
     // insert user role details
     @PostMapping("/insertRole")
     public ResponseEntity<String> InsertRole(@RequestBody UserRole userRole){
         userDetailsService.InsertRole(userRole);
-//        System.out.println(userRole.getRoleCode());
         String message = "successfully insert user Role";
-        return ResponseEntity.ok(message);
-//        return ResponseEntity.status(HttpStatus.OK).body(responseData);
-    }
 
+        return ResponseEntity.status(HttpStatus.OK).body(message);
+    }
+    // Retrieve the all user roles
+    @GetMapping("getAllUserRoles")
+    public List <UserRole> AllUserRoles (){
+        return userRoleRepository.findAll() ;
+    }
 }
