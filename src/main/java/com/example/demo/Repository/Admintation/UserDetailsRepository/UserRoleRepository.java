6359e26dbbcb6a12e34621d799d392b97e49cbe4
pramodBhanaka <bhanaka94pramod@gmail.com>
2023-09-26 15:45:56 +0530

    add the repository annotataion for the repo class

--- src/main/java/com/example/demo/Repository/Admintation/UserDetailsRepository/UserRoleRepository.java
+++ src/main/java/com/example/demo/Repository/Admintation/UserDetailsRepository/UserRoleRepository.java
@@ -6,7 +6,9 @@ import com.example.demo.Models.ForestLetter.LetterDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
     @Modifying
     @Query(value = " UPDATE `user_role` " +
